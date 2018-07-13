88486eed344ac4999d2c2e9ce01320a22bccd7cb
Vincent Castelli <vincentsalvatorecastelli@gmail.com>
2018-07-13 11:48:12 -0700

    Initial project setup.

---
+++ challenge_3/webpack.config.js
@@ -0,0 +1,68 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const srcDir = path.join(__dirname, '/client/src');
+const distDir = path.join(__dirname, '/public/dist');
+
+module.exports = {
+  mode: 'development',
+  entry: `${srcDir}/index.jsx`,
+  resolve: { extensions: ['.js', '.jsx'] },
+  output: {
+    path: distDir,
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader'],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: { minimize: true },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.png$/,
+        use: 'url-loader?limit=100000',
+      },
+      {
+        test: /\.jpg$/,
+        use: 'file-loader',
+      },
+      {
+        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+        use: 'url-loader? limit=10000&mimetype=application/font-woff',
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        use: 'url-loader?limit=10000&mimetype=application/octet-stream',
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        use: 'file-loader',
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        use: 'url-loader?limit=10000&mimetype=image/svg+xml',
+      },
+    ],
+  }
+}
